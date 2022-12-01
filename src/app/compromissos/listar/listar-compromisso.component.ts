4134a1fa2837bbab18d7deb4755894213efb8701
Tiago <tiagosantiniathayde@gmail.com>
2022-12-01 12:31:17 -0300

    Implementando input para período de tempo

--- src/app/compromissos/listar/listar-compromisso.component.ts
+++ src/app/compromissos/listar/listar-compromisso.component.ts
@@ -1,7 +1,8 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
-import { Observable } from 'rxjs';
+import { debounceTime, Observable } from 'rxjs';
 import { BaseCardListComponent } from 'src/app/shared/base-card-list/base-card-list.component';
 import { CompromissoService } from '../services/compromisso.service';
 import { ListarCompromissoViewModel } from '../view-models/listar-compromisso.view-model';
@@ -12,10 +13,11 @@ import { ListarCompromissoViewModel } from '../view-models/listar-compromisso.vi
 })
 export class ListarCompromissoComponent
   extends BaseCardListComponent
-  implements OnInit {
+  implements OnInit, AfterViewInit {
 
   compromissos$: Observable<ListarCompromissoViewModel[]>;
   filtrosCompromissos: string[] = ['Hoje', 'Futuros', 'Passados'];
+  periodoFuturo: FormControl = new FormControl(7);
 
   constructor(
     titulo: Title,
@@ -30,10 +32,22 @@ export class ListarCompromissoComponent
     this.compromissos$ = this.compromissoService.selecionarCompromissosDeHoje();
   }
 
+  ngAfterViewInit(): void {
+    this.periodoFuturo.valueChanges
+      .pipe(debounceTime(300))
+      .subscribe((periodo) => {
+        this.compromissos$ = this.compromissoService.selecionarCompromissosFuturos(periodo)
+      });
+  }
+
   trocarFiltro(filtroSelecionado: number) {
     switch (filtroSelecionado) {
       default: this.compromissos$ = this.compromissoService.selecionarCompromissosDeHoje(); break;
-      case 1: this.compromissos$ = this.compromissoService.selecionarCompromissosFuturos(); break;
+      case 1: {
+        this.periodoFuturo.setValue(7);
+        this.compromissos$ = this.compromissoService.selecionarCompromissosFuturos();
+        break
+      };
       case 2: this.compromissos$ = this.compromissoService.selecionarCompromissosPassados(); break;
     }
   }
