d07db87cb79e81efcee67fab42364139f006cb4b
Tiago <tiagosantiniathayde@gmail.com>
2022-12-05 14:17:26 -0300

    Alterações de design

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -14,6 +14,7 @@ import { LoginGuard } from './services/login.guard';
 import { RegistroComponent } from './registro/registro.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
@@ -31,6 +32,7 @@ import { SharedModule } from '../shared/shared.module';
     MatButtonModule,
     MatIconModule,
     MatInputModule,
+    MatToolbarModule,
 
     ReactiveFormsModule,
     HttpClientModule,
